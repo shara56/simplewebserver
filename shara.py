09e1c29af62980c65cd3299c955597cb1b64d738
shara56 <sharanginik@gmail.com>
2023-03-24 09:46:49 +0000

    Sucess Shara

--- shara.py
+++ shara.py
@@ -1,20 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -43,11 +26,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-
-
-## OUTPUT:
-![OUTPUT](./out1.png)
-![OUTPUT](./out2.png)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
